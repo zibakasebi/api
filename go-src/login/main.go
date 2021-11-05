1983ac87793bb95f201961cc94167a0f909fa9dc
Esmaeil mazahery <smaeil.mazahery@gmail.com>
2021-11-05 10:11:27 +0330

    up

--- go-src/login/main.go
+++ go-src/login/main.go
@@ -15,7 +15,22 @@ type LoginRequest struct {
 	Password string `json:"password"`
 }
 
+func setupResponseCors() (*events.APIGatewayProxyResponse, error) {
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE",
+			"Access-Control-Allow-Headers": "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization",
+		},
+	}, nil
+}
+
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	if request.HTTPMethod == "OPTIONS" {
+		return setupResponseCors()
+	}
+
 	//convert string to struct
 	var loginRequest LoginRequest
 	err := json.Unmarshal([]byte(request.Body), &loginRequest)
